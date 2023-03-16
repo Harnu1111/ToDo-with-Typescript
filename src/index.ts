31db009dfd2532f7ca6ab027e70b47a43d03f795
Anuoluwapo <jaguarjum@gmail.com>
2023-03-16 17:30:17 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const input = document.getElementById("input") as HTMLInputElement;
+const btn = document.getElementById("button") as HTMLButtonElement;
+const list = document.getElementById("list") as HTMLUListElement;
 
-import confetti from 'canvas-confetti';
+btn.addEventListener("click", (e) => {
+e.preventDefault()
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+  const task = document.createElement("li");
+  const item = document.createElement("item");
+  const label = document.createElement("label");
+  const check = document.createElement("input");
+  const deletebtn = document.createElement("button");
+
+  check.type = "checkbox";
+
+  deletebtn.innerHTML = "Delete";
+  deletebtn.classList.add("deleteBtn");
+
+  item.innerHTML = input.value;
+  label.append(check, item, deletebtn);
+  task.append(label);
+  list.append(task);
+
+  deletebtn.addEventListener("click", () => {
+    task.remove();
+  });
+
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      task.style.textDecoration = "line-through";
+    } else {
+      task.style.textDecoration = "none";
+    }
+  });
+
+  input.value = "";
+});
+
+
